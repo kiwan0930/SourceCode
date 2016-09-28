cb461d1ed0e4bc55ee610085f25d0b195119360b
FocusKiwan <kiwanboy@naver.com>
2016-09-28 09:16:01 +0900

    String -> Hello World

--- src/SourceCode.java
+++ src/SourceCode.java
@@ -1,8 +1,8 @@
 
 public class SourceCode {
 	public static void main(String[] args){
-		System.out.println("Source Code First");
-		System.out.println("Source Code Second");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 		
 	}
 }
