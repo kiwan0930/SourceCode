9f10dec1a6b818c6488d4fc565a3398d39d6f671
FocusKiwan <kiwanboy@naver.com>
2016-09-28 09:49:14 +0900

    this is SourceCode22222

--- src/SourceCode.java
+++ src/SourceCode.java
@@ -1,15 +1,8 @@
 
 public class SourceCode {
 	public static void main(String[] args){
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-		
-		for(int i=0 ; i <3; i++)
-			print("print Fucntion");
-		
-	}
-	
-	public static void print(String str){
-		System.out.println(str);
+		System.out.println("This is SourceCode2");
+		System.out.println("This is SourceCode2");
+			
 	}
 }
